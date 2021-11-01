e36fc64d062cbd4575aa3106b72310344863bf06
bucky <buckyroberts@gmail.com>
2021-11-01 17:28:30 -0400

    Cleaning

--- config/settings/base.py
+++ config/settings/base.py
@@ -47,13 +47,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
