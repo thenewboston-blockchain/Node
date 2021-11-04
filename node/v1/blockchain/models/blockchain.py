43fe8a4008d26baf6aa00fbf05980c7c71144280
bucky <buckyroberts@gmail.com>
2021-11-04 18:27:46 -0400

    Replacing Redis with Mongo

--- node/v1/blockchain/models/blockchain.py
+++ node/v1/blockchain/models/blockchain.py
@@ -1,10 +1,7 @@
 from dataclasses import asdict
 
-from django.core.cache import cache
-
 from v1.block_messages.types import BlockMessageTypes
 from v1.blockchain.models.mongo import Mongo
-from v1.cache.cache_keys import get_account_balance_cache_key, get_account_lock_cache_key
 
 
 class Blockchain:
@@ -13,26 +10,7 @@ class Blockchain:
         self.mongo = Mongo()
 
     def add(self, *, block_message: BlockMessageTypes):
-        self.update_cache(updates=block_message.updates)
         self.mongo.insert_block(block_message=asdict(block_message))
-
-    def update_cache(self, *, updates):
-        self.update_cached_accounts(accounts=updates['accounts'])
-
-    @staticmethod
-    def update_cached_accounts(*, accounts):
-        for account_number, account_data in accounts.items():
-            balance = account_data.get('balance')
-            lock = account_data.get('lock')
-
-            if balance:
-                account_balance_cache_key = get_account_balance_cache_key(account_number=account_number)
-                cache.set(account_balance_cache_key, balance, None)
-
-            if lock:
-                account_lock_cache_key = get_account_lock_cache_key(account_number=account_number)
-                cache.set(account_lock_cache_key, lock, None)
 
     def reset(self):
         self.mongo.reset_blockchain()
-        cache.clear()
--- node/v1/cache/cache_keys.py
+++
@@ -1,6 +0,0 @@
-def get_account_balance_cache_key(*, account_number):
-    return f'account:{account_number}:balance'
-
-
-def get_account_lock_cache_key(*, account_number):
-    return f'account:{account_number}:lock'
