cfa4a9ab9570b1a3a281283d2ca3fc2ea4913fa4
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-11-11 17:37:42 +0300

    Standardizing fixups

--- node/core/utils/tests/test_tools.py
+++ node/core/utils/tests/test_tools.py
@@ -2,9 +2,4 @@ from ..tools import sort_and_encode
 
 
 def test_sort_and_encode():
-    obj = {
-       'b': 'value',
-       'c': 'value',
-       'a': 'value',
-    }
-    assert sort_and_encode(obj) == b'{"a":"value","b":"value","c":"value"}'
+    assert sort_and_encode({'b': 'value', 'c': 'value', 'a': 'value'}) == b'{"a":"value","b":"value","c":"value"}'
