d3b72b94a5078013a0c625e7940d079633c2fc0f
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-11-16 21:49:41 +0300

    Fixup

--- node/config/tests/test_settings.py
+++ node/config/tests/test_settings.py
@@ -11,4 +11,4 @@ def test_settings_smoke():
 
 @pytest.mark.skipif(UNITTEST_ENV_VAR_NAME not in os.environ, reason=f'{UNITTEST_ENV_VAR_NAME} env var is not set')
 def test_env_var_override():
-    assert getattr(settings, UNITTEST_ENV_VAR_NAME.removeprefix('NODE_'), None) == {1: 'for unittests'}  # noqa: B009
+    assert getattr(settings, UNITTEST_ENV_VAR_NAME.removeprefix('NODE_'), None) == {'test': 1}  # noqa: B009
