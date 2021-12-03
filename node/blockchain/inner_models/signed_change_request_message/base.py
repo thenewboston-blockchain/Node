6649a4fbc258bd3dfe5c31684f5689bf59782f6d
Oleg Tropinin <oleg@iptech.kz>
2021-12-04 03:05:09 +0600

    BC-162 Make enum strict, so coerce strings are not allowed.

--- node/blockchain/inner_models/signed_change_request_message/base.py
+++ node/blockchain/inner_models/signed_change_request_message/base.py
@@ -5,6 +5,4 @@ from node.blockchain.types import AccountLock, Type
 
 class SignedChangeRequestMessage(BaseModel, SignableMixin):
     account_lock: AccountLock
-    # TODO(dmu) MEDIUM: Maybe `type` type should be defined as `int` instead of `IntEnum` for simpler serialization
-    #                   (let's see how it works with Django REST Framework)
     type: Type  # noqa: A003
