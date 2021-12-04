56b022e0f2381da9eabe6acea2af6ec5828f5d02
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-12-04 16:53:59 +0300

    Add node declaration block

--- node/blockchain/tests/test_models/test_block/test_node_declaration.py
+++ node/blockchain/tests/test_models/test_block/test_node_declaration.py
@@ -1,5 +1,6 @@
 import pytest
 
+from node.blockchain.facade import BlockchainFacade
 from node.blockchain.inner_models import BlockMessage
 from node.blockchain.models.block import Block
 from node.core.utils.cryptography import is_signature_valid
@@ -7,9 +8,14 @@ from node.core.utils.types import Signature, Type
 
 
 @pytest.mark.django_db
-def test_create_from_block_message(genesis_block_message, primary_validator_key_pair):
+def test_create_from_block_message(node_declaration_block_message, primary_validator_key_pair):
+    blockchain_facade = BlockchainFacade.get_instance()
+
+    expected_block_number = blockchain_facade.get_next_block_number()
+    expected_identifier = blockchain_facade.get_next_block_identifier()
+
     block = Block.objects.add_block_from_block_message(
-        message=genesis_block_message,
+        message=node_declaration_block_message,
         signing_key=primary_validator_key_pair.private,
         validate=False,
     )
@@ -18,18 +24,18 @@ def test_create_from_block_message(genesis_block_message, primary_validator_key_
     assert isinstance(block.signature, str)
     assert is_signature_valid(block.signer, block.message.encode('utf-8'), Signature(block.signature))
     message = BlockMessage.parse_raw(block.message)
-    assert message.number == 0
-    assert message.identifier is None
-    assert message.type == Type.GENESIS
-    assert message == genesis_block_message
+    assert message.number == expected_block_number
+    assert message.identifier == expected_identifier
+    assert message.type == Type.NODE_DECLARATION
+    assert message == node_declaration_block_message
 
-    block = Block.objects.get(_id=0)
+    block = Block.objects.get(_id=1)
     assert block.signer == primary_validator_key_pair.public
     assert isinstance(block.message, str)
     assert isinstance(block.signature, str)
     assert is_signature_valid(block.signer, block.message.encode('utf-8'), Signature(block.signature))
     message = BlockMessage.parse_raw(block.message)
-    assert message.number == 0
-    assert message.identifier is None
-    assert message.type == Type.GENESIS
-    assert message == genesis_block_message
+    assert message.number == expected_block_number
+    assert message.identifier == expected_identifier
+    assert message.type == Type.NODE_DECLARATION
+    assert message == node_declaration_block_message
