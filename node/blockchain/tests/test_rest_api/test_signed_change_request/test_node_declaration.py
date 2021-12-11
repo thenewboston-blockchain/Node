98b1b8cf45629fb5c61ae6aeabe395a80d43ead9
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2021-12-11 21:57:00 +0300

    Unittest implementation

--- node/blockchain/tests/test_rest_api/test_signed_change_request/test_node_declaration.py
+++ node/blockchain/tests/test_rest_api/test_signed_change_request/test_node_declaration.py
@@ -1,6 +1,9 @@
 import pytest
 
 from node.blockchain.inner_models import NodeDeclarationSignedChangeRequestMessage, SignedChangeRequest
+from node.blockchain.tests.test_models.base import (
+    CREATE, VALID, node_declaration_message_type_api_validation_parametrizer
+)
 from node.core.utils.collections import deep_update
 
 
@@ -26,13 +29,34 @@ def test_node_declaration_signed_change_request_can_be_sent_via_api(api_client, 
         api_client.post('/api/signed-change-request/', payload)
 
 
-@pytest.mark.skip('Not implemented yet')
-def test_type_validation_for_node_declaration():
-    # TODO(dmu) HIGH: Implement similar to `node.blockchain.tests.test_models.test_signed_change_request.
-    #                 test_node_declaration.test_type_validation_for_node_declaration_message_on_parsing`
-    #                 !!! Reuse the same input data
-    #                 https://thenewboston.atlassian.net/browse/BC-170
-    raise NotImplementedError
+@pytest.mark.django_db
+@node_declaration_message_type_api_validation_parametrizer
+def test_type_validation_for_node_declaration(
+    id_, regular_node, node, node_addresses, node_fee, account_lock, expected_response_body, api_client
+):
+    regular_node_dict = regular_node.dict()
+    del regular_node_dict['identifier']
+    payload = {
+        'signer': '0' * 64,
+        'signature': '0' * 128,
+        'message': {
+            'type':
+                1,
+            'account_lock':
+                regular_node.identifier if account_lock is VALID else account_lock,
+            'node':
+                regular_node_dict if node is VALID else ({
+                    'addresses': regular_node.addresses if node_addresses is VALID else node_addresses,
+                    'fee': regular_node.fee if node_fee is VALID else node_fee,
+                } if node is CREATE else node)
+        }
+    }
+    response = api_client.post('/api/signed-change-request/', payload)
+    assert response.status_code == 400
+    assert response.json() == expected_response_body or response.json() == dict(
+        expected_response_body,
+        non_field_errors=['Invalid signature'],
+    )
 
 
 @pytest.mark.django_db
@@ -52,10 +76,7 @@ def test_signature_validation_for_node_declaration(update_with, api_client, regu
         message=message,
         signing_key=regular_node_key_pair.private,
     )
-    payload = signed_change_request.dict()
-
-    wrong_payload = deep_update(payload, update_with)
-
-    response = api_client.post('/api/signed-change-request/', wrong_payload)
+    payload = deep_update(signed_change_request.dict(), update_with)
+    response = api_client.post('/api/signed-change-request/', payload)
     assert response.status_code == 400
     assert response.json() == {'non_field_errors': ['Invalid signature']}
