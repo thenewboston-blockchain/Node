75815659fd0f01f7848d6082bc18b28ade7bcef8
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2022-01-25 20:32:06 +0300

    get_nodes_majority() draft

--- node/blockchain/management/commands/sync_blockchain_with_network.py
+++ node/blockchain/management/commands/sync_blockchain_with_network.py
@@ -5,4 +5,5 @@ class Command(BaseCommand):
     help = 'Sync local blockchain with thenewboston blockchain network'  # noqa: A003
 
     def handle(self, *args, **options):
+        # TODO(dmu) CRITICAL: Implement in https://thenewboston.atlassian.net/browse/BC-164
         raise NotImplementedError
--- node/blockchain/utils/network.py
+++ node/blockchain/utils/network.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from typing import Optional
+
 from node.blockchain.inner_models.node import Node
 
 
@@ -8,3 +11,46 @@ def get_nodes_for_syncing() -> list[Node]:
     #                     Otherwise use nodes from JSON-file (stored during docker image build)
     #                     Otherwise return an empty list of nodes
     return []
+
+
+def get_node_last_block_number(node):
+    raise NotImplementedError
+
+
+def get_node_block_hash(node, block_number):
+    raise NotImplementedError
+
+
+def enrich_with_last_block_number(nodes):
+    for node in nodes:
+        node.last_block_number = get_node_last_block_number(node)
+
+
+def get_nodes_majority(nodes: list[Node]) -> Optional[list[Node]]:
+    # TODO(dmu) CRITICAL: Implement in https://thenewboston.atlassian.net/browse/BC-186
+    clusters = defaultdict(set)
+    enrich_with_last_block_number(nodes)
+    # TODO(dmu) CRITICAL: Filter out offline nodes
+    filtered_nodes = nodes
+    sorted_nodes = sorted(filtered_nodes, key=lambda x: x.last_block_number)
+    for node_index in range(len(sorted_nodes)):
+        node = sorted_nodes[node_index]
+        block_number = node.last_block_number
+
+        block_hash = get_node_block_hash(node, block_number)
+        clusters[(block_number, block_hash)].add(node)
+        for other_node_index in range(node_index + 1, len(sorted_nodes)):
+            other_node = sorted_nodes[other_node_index]
+            block_hash = get_node_block_hash(other_node, block_number)
+            clusters[(block_number, block_hash)].add(other_node)
+
+    majority_count = len(nodes) // 2 + 1
+
+    clusters_sorted = sorted(((key[0], key[1], len(value)) for key, value in clusters.items()), key=lambda x: x[2])
+    for cluster_sorted in clusters_sorted:
+        if cluster_sorted[2] < majority_count:
+            continue
+
+        return list(clusters[(cluster_sorted[0], cluster_sorted[1])])
+
+    return None
