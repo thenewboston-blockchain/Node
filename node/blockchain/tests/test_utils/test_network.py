425ca64f3d219954ae0d751888f2194eec6d42a1
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2022-01-27 21:30:19 +0300

    test_clusterize_nodes_and_get_best_cluster()

--- node/blockchain/tests/test_utils/test_network.py
+++ node/blockchain/tests/test_utils/test_network.py
@@ -1,10 +1,12 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 
+from node.blockchain.inner_models import Node
 from node.blockchain.models import Block
-from node.blockchain.utils.network import get_node_block, node_block_cache
+from node.blockchain.utils.network import clusterize_nodes, get_best_cluster, get_node_block, node_block_cache
 from node.core.clients.node import NodeClient
+from node.core.utils.misc import Wrapper
 
 
 @pytest.mark.parametrize('block_number', (1, 'last'))
@@ -36,3 +38,88 @@ def test_get_node_block(test_server_address_regular_node, block_number):
     mock.assert_not_called()
     assert block == expected_block
     assert node_block_cache == expect_cache
+
+
+def test_clusterize_nodes_and_get_best_cluster():
+
+    def make_block(block_number):
+        return MagicMock(
+            **{
+                'get_block_number.return_value': block_number,
+                'make_hash.return_value': str(block_number)
+            }
+        )
+
+    node_clusters_1_2_3 = Wrapper(Node(identifier='0' * 64, addresses=['http://n0/'], fee=0), last_block=make_block(3))
+
+    block_2 = make_block(2)
+    node_1_clusters_1_2 = Wrapper(
+        Node(identifier='1' * 64, addresses=['http://n1/'], fee=0),
+        last_block=block_2,
+    )
+
+    node_2_clusters_1_2 = Wrapper(
+        Node(identifier='2' * 64, addresses=['http://n2/'], fee=0),
+        last_block=block_2,
+    )
+
+    node_3_clusters_1_2 = Wrapper(
+        Node(identifier='3' * 64, addresses=['http://n3/'], fee=0),
+        last_block=block_2,
+    )
+
+    node_clusters_1 = Wrapper(Node(identifier='4' * 64, addresses=['http://n4/'], fee=0), last_block=make_block(1))
+
+    node_clusters_4 = Wrapper(
+        Node(identifier='5' * 64, addresses=['http://n5/'], fee=0),
+        last_block=MagicMock(**{
+            'get_block_number.return_value': 3,
+            'make_hash.return_value': 'another_3',
+        })
+    )
+
+    cluster1 = [
+        node_clusters_1.body, node_1_clusters_1_2.body, node_3_clusters_1_2.body, node_clusters_1_2_3.body,
+        node_2_clusters_1_2.body
+    ]
+    cluster2 = [node_clusters_1_2_3.body, node_1_clusters_1_2.body, node_2_clusters_1_2.body, node_3_clusters_1_2.body]
+    cluster3 = [node_clusters_1_2_3.body]
+    cluster4 = [node_clusters_4.body]
+    nodes = [
+        # List is randomized on purpose: for better test coverage on random data
+        node_1_clusters_1_2,
+        node_clusters_1,
+        node_3_clusters_1_2,
+        node_clusters_1_2_3,
+        node_2_clusters_1_2,
+        node_clusters_4
+    ]
+
+    def get_block_node(node, block_number):
+        if node.identifier == node_clusters_4.body.identifier:
+            return MagicMock(
+                **{
+                    'get_block_number.return_value': block_number,
+                    'make_hash.return_value': f'another_{block_number}',
+                }
+            )
+
+        return make_block(block_number)
+
+    with patch('node.blockchain.utils.network.get_node_block', new=get_block_node):
+        clusters = clusterize_nodes(nodes)
+
+    assert len(clusters) == 4
+
+    def cluster_to_node_identifiers(cluster):
+        return tuple(sorted(node.identifier for node in cluster))
+
+    actual_clusters = tuple(sorted(cluster_to_node_identifiers(cluster[1]) for cluster in clusters))
+    expected_clusters = tuple(
+        sorted(cluster_to_node_identifiers(cluster) for cluster in (cluster1, cluster2, cluster3, cluster4))
+    )
+    assert expected_clusters == actual_clusters
+
+    best_cluster = get_best_cluster(clusters, 4)
+    # cluster2 is the best because it has the longest blockchain among clusters satisfying majority of nodes
+    assert set(node.identifier for node in cluster2) == set(node.identifier for node in best_cluster)
--- node/blockchain/utils/network.py
+++ node/blockchain/utils/network.py
@@ -5,6 +5,7 @@ from typing import Optional, Union
 
 from node.blockchain.inner_models import Block, Node
 from node.core.clients.node import NodeClient
+from node.core.utils.misc import Wrapper
 
 from ..constants import LAST_BLOCK_ID
 
@@ -34,47 +35,66 @@ def get_node_block(node: Node, block_number: Union[int, str]) -> Optional[Block]
     return block
 
 
-def get_available_nodes(nodes: list[Node]):
+def get_available_nodes(nodes: list[Node]) -> list[Wrapper]:
     available_nodes = []
     for node in nodes:
         last_block = get_node_block(node, LAST_BLOCK_ID)
         if last_block is None:
             continue
 
-        node.last_block = last_block
-        available_nodes.append(node)
+        available_nodes.append(Wrapper(node, last_block=last_block))
 
     return available_nodes
 
 
-def clusterize_nodes(nodes):
+def clusterize_nodes(node_wrappers: list[Wrapper]):
     clusters = defaultdict(set)
-    nodes = sorted(nodes, key=lambda _node: _node.last_block.get_block_number())
-    for node_index, node in enumerate(nodes):
-        last_block = node.last_block
-        last_block_hash = last_block.make_hash()
 
+    # Sort nodes, so we start from the longest blockchain
+    node_wrappers = sorted(node_wrappers, key=lambda wrapper: wrapper.last_block.get_block_number())  # type: ignore
+    for node_index, node_wrapper in enumerate(node_wrappers):
+        last_block = node_wrapper.last_block  # type: ignore
         block_number = last_block.get_block_number()
-        clusters[(block_number, last_block_hash)].add(node)
 
-        for other_node in islice(nodes, node_index + 1, len(nodes)):
-            block = get_node_block(other_node, block_number)
+        # Add node to cluster (either to existing or making new one)
+        clusters[(block_number, last_block.make_hash())].add(node_wrapper)
+
+        # See what hashes for the same block number do other nodes have
+        for other_node_wrapper in islice(node_wrappers, node_index + 1, len(node_wrappers)):
+            block = get_node_block(other_node_wrapper.body, block_number)
             if block is None:
                 continue
 
             assert block.get_block_number() == block_number
 
-            clusters[(block_number, block.make_hash())].add(other_node)
+            # Add node to cluster (either to existing or making new one)
+            clusters[(block_number, block.make_hash())].add(other_node_wrapper)
 
-    return clusters
+    # Remove identical clusters (having exactly same nodes)
+    reduced_clusters = set()
+    for cluster in clusters.values():
+        reduced_clusters.add(tuple(sorted(wrapper.body.identifier for wrapper in cluster)))
+
+    # Produce clusters of nodes
+    wrapper_by_identifier = {wrapper.body.identifier: wrapper for wrapper in node_wrappers}
+    node_clusters = []
+    for cluster in reduced_clusters:  # type: ignore
+        block_number = min(
+            wrapper_by_identifier[node_identifier].last_block.get_block_number()  # type: ignore
+            for node_identifier in cluster
+        )
+        nodes = tuple(wrapper_by_identifier[node_identifier].body for node_identifier in cluster)
+        node_clusters.append((block_number, nodes))
+
+    return node_clusters
 
 
 def get_best_cluster(clusters, majority_count):
-    clusters = list(filter(lambda cluster: len(cluster) >= majority_count, clusters))
+    clusters = list(filter(lambda cluster: len(cluster[1]) >= majority_count, clusters))
     if not clusters:
         return None
 
-    return max(clusters, key=lambda cluster: len(cluster))
+    return max(clusters, key=lambda cluster: cluster[0])[1]
 
 
 def clear_cache(func):
@@ -93,7 +113,9 @@ def clear_cache(func):
 @clear_cache
 def get_nodes_majority(nodes: list[Node]) -> Optional[list[Node]]:
     available_nodes = get_available_nodes(nodes)
-    majority_count = len(available_nodes) // 2 + 1
+    if not available_nodes:
+        return None
 
-    clusters = list(clusterize_nodes(available_nodes).values())
-    return list(get_best_cluster(clusters, majority_count))
+    majority_count = len(available_nodes) // 2 + 1
+    clusters = clusterize_nodes(available_nodes)
+    return get_best_cluster(clusters, majority_count) or None
