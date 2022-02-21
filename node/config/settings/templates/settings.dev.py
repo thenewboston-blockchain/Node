456b4cd59a75bdc6b8aab1fd264d4aa8f787eb3a
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2022-02-21 21:09:35 +0300

    Traceback suppressed in warnings

--- node/config/settings/templates/settings.dev.py
+++ node/config/settings/templates/settings.dev.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'django-insecure-^m3d4yj1zic931t3z_b()(xz-_34c3sjeh_4v41rf-2j8qs'
 
 MIDDLEWARE += ('node.core.middleware.LoggingMiddleware',)
 LOGGING['formatters']['colored'] = {
-    '()': 'colorlog.ColoredFormatter',
+    '()': 'node.core.utils.formatters.TracebackSuppressingColoredFormatter',
     'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
 }
 LOGGING['loggers']['node']['level'] = 'DEBUG'
